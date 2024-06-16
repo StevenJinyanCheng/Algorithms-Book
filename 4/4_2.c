511a0407daac69b0dc4c30b4228114e378bb123d
StevenJinyanCheng <steven.pi.cheng@outlook.com>
2024-06-17 07:24:02 +0800

    Do single-test-case 4.2 quetion

--- 4/4_2.c
+++ 4/4_2.c
@@ -1,22 +1,50 @@
 #include <stdio.h>
-int Hs[400][2];
-int Vs[400][2];
-int main(int argc, const char *argv[]){
+#include <stdbool.h>
+
+bool Hs[15][15];
+bool Vs[15][15];
+
+int main(int argc, const char *argv[]) {
     int n, m;
     scanf("%d%d", &n, &m);
-    for(int i = 1;i <= m;i++){
+    for (int i = 1; i <= m; i++) {
         char p;
         int a, b;
-        scanf("%c %d %d", &p, &a, &b);
-        if(p == 'H'){
-            Hs[i][0] = a;
-            Hs[i][1] = b;
+        scanf(" %c %d %d", &p, &a, &b);
+        if (p == 'H') {
+            Hs[a][b] = true;
         }
-        if(p == 'V'){
-            Vs[i][0] = a;
-            Vs[i][1] = b;
+        if (p == 'V') {
+            Vs[a][b] = true;
         }
     }
-    
+
+    bool foundAnySquares = false;
+    for (int size = 1; size <= n; size++) {
+        int count = 0;
+        for (int i = 1; i <= n - size; i++) {
+            for (int j = 1; j <= n - size; j++) {
+                bool flag = true;
+                for (int k = 0; k < size; k++) {
+                    if (!Hs[i][j + k] || !Hs[i + size][j + k] || !Vs[i + k][j] || !Vs[i + k][j + size]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    count++;
+                }
+            }
+        }
+        if (count > 0) {
+            printf("%d square(s) with size %d\n", count, size);
+            foundAnySquares = true;
+        }
+    }
+
+    if (!foundAnySquares) {
+        printf("No completed squares can be found.\n");
+    }
+
     return 0;
 }
